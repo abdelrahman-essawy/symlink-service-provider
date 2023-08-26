b49307916603f4c85004e406a658ab2f4704e722
Mohamed Tarek <m.tarekazab1997@gmail.com>
2023-08-27 02:56:47 +0300

    remove unneeded bussiness logic

--- src/@types/filter.tsx
+++
@@ -1,6 +0,0 @@
-export interface Filter {
-  key: string;
-  value: string;
-  end?: string;
-  operator?: string;
-}
--- src/utils/apply-pagination.tsx
+++
@@ -1,3 +0,0 @@
-export function applyPagination(documents: any[], page: number, rowsPerPage: number) {
-  return documents.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage);
-}
