1799c0a593eaef69cd8eb32488876a9c7d909cda
Mousa Ahmed <moussa.abdelghany@gmail.com>
2023-12-13 13:39:57 +0200

    style/Edit Auth UI

--- src/pages/auth/register.tsx
+++ src/pages/auth/register.tsx
@@ -2,12 +2,13 @@ import Head from "next/head";
 import NextLink from "next/link";
 import { useRouter } from "next/navigation";
 import { useFormik } from "formik";
-import PeopleAltOutlinedIcon from '@mui/icons-material/PeopleAltOutlined';
+import PeopleAltOutlinedIcon from "@mui/icons-material/PeopleAltOutlined";
 import * as Yup from "yup";
 import {
   Box,
   Button,
-  Link,
+  FormControlLabel,
+  Radio,
   Stack,
   TextField,
   Typography,
@@ -28,10 +29,12 @@ import PhotoCameraIcon from "@mui/icons-material/PhotoCamera";
 import PersonIcon from "@mui/icons-material/Person";
 import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 import VisibilityIcon from "@mui/icons-material/Visibility";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControl from "@mui/material/FormControl";
 const Page = () => {
   const router = useRouter();
   const auth = useAuth();
-  const { t ,i18n} = useTranslation();
+  const { t, i18n } = useTranslation();
   const [selectedFile, setSelectedFile] = useState<any>(null);
   const [previewUrl, setPreviewUrl] = useState<any>(null);
   const [showPassword, setShowPassword] = useState(false);
@@ -47,11 +50,11 @@ const Page = () => {
   const Roles = [
     {
       id: "1",
-      name: "PROVIDER",
+      name: "Expert",
     },
     {
       id: "2",
-      name: "CLIENT",
+      name: "Company",
     },
   ];
   const formik = useFormik({
@@ -110,34 +113,34 @@ const Page = () => {
             </Stack> */}
             <form noValidate onSubmit={formik.handleSubmit}>
               <Stack spacing={3}>
-              <Grid item xs={12} sx={{ display: 'flex', justifyContent: 'center' }}>
-                <Badge
-                  overlap="circular"
-                  anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
-                  badgeContent={
-                    <Avatar
-                      alt="New Account"
-                      src="/static/images/avatar/2.jpg"
-                      sx={{ boxShadow: "-6px 7px 8px rgba(0, 0, 0, 0.08)" }}
-                    >
-                      <Button
-                        variant="contained"
-                        size="large"
-                        component="label"
-                        sx={{ color: "white" }}
+                <Grid item xs={12} sx={{ display: "flex", justifyContent: "center" }}>
+                  <Badge
+                    overlap="circular"
+                    anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+                    badgeContent={
+                      <Avatar
+                        alt="New Account"
+                        src="/static/images/avatar/2.jpg"
+                        sx={{ boxShadow: "-6px 7px 8px rgba(0, 0, 0, 0.08)" }}
                       >
-                        <PhotoCameraIcon fontSize="small" />
-                        <input type="file" onChange={handleFileSelect} hidden />
-                      </Button>
-                    </Avatar>
-                  }
-                >
-                  <Avatar
-                    sx={{ width: 100, height: 100 }}
-                    alt="Travis Howard"
-                    src={previewUrl}
-                  ></Avatar>
-                </Badge>
+                        <Button
+                          variant="contained"
+                          size="large"
+                          component="label"
+                          sx={{ color: "white" }}
+                        >
+                          <PhotoCameraIcon fontSize="small" />
+                          <input type="file" onChange={handleFileSelect} hidden />
+                        </Button>
+                      </Avatar>
+                    }
+                  >
+                    <Avatar
+                      sx={{ width: 100, height: 100 }}
+                      alt="Travis Howard"
+                      src={previewUrl}
+                    ></Avatar>
+                  </Badge>
                 </Grid>
                 <TextField
                   error={!!(formik.touched.email && formik.errors.email)}
@@ -177,55 +180,34 @@ const Page = () => {
                   autoComplete="off"
                 />
 
-                <FormLabel sx={{ mx: 0,direction:i18n.language == 'ar' ? "ltr":"rtl" }}>{t("Register as")}</FormLabel>
-                <Select
-                  labelId="demo-multiple-name-label"
-                  id="demo-multiple-name"
-                  fullWidth={true}
-                  sx={{
-                    my: 1,
-                    borderRadius: "50px",
-                    "& .muirtl-19j8lcu-MuiSelect-select-MuiInputBase-input-MuiOutlinedInput-input.MuiSelect-select":
-                      {
-                        borderRadius: "100px",
-                        height: "20.125px",
-                      },
-                    "& .MuiSelect-select ,& .MuiSelect-outlined ,& .MuiInputBase-input ,& .MuiOutlinedInput-input ,& .muirtl-wnioi0-MuiSelect-select-MuiInputBase-input-MuiOutlinedInput-input":
-                      {
-                        margin: '0 30px'
-
-                      },
-                  }}
-                  MenuProps={{
-                    PaperProps: {
-                      style: {
-                        maxHeight: 150,
-                      },
-                    },
-                  }}
-                  variant="outlined"
-                  value={formik.values.role}
-                  name="role"
-                  onBlur={formik.handleBlur}
-                  onChange={formik.handleChange}
-                  IconComponent={() => (
-                    <PersonIcon
-                      style={{
-                        position: "absolute",
-                        color: "grey",
-                        userSelect: "none",
-                        pointerEvents: "none",
-                        margin: "15px",
-                      }}
-                    />
-                  )}
-                >
-                  {Roles.map((item: any) => (
-                    <MenuItem key={item?.id} value={item?.name}>
-                      {t(item?.name)}
-                    </MenuItem>
-                  ))}
-                </Select>
+                <Grid item xs={12}>
+                  <FormControl fullWidth>
+                    <RadioGroup
+                      row
+                      value={formik.values.role}
+                      name="role"
+                      onBlur={formik.handleBlur}
+                      onChange={formik.handleChange}
+                    >
+                      <Grid container>
+                        {Roles?.length &&
+                          Roles?.map((role: any) => {
+                            return (
+                              <Grid item xs={6} md={4} lg={3} key={role?.id}>
+                                <FormControlLabel
+                                  sx={{ width: "100%" }}
+                                  value={role?.id}
+                                  control={<Radio color="warning" />}
+                                  label={t(role?.name)}
+                                  color="warning"
+                                />
+                              </Grid>
+                            );
+                          })}
+                      </Grid>
+                    </RadioGroup>
+                  </FormControl>
+                </Grid>
               </Stack>
               {formik.errors.submit && (
                 <Typography color="error" sx={{ mt: 3 }} variant="body2">
@@ -235,10 +217,10 @@ const Page = () => {
               <Button
                 fullWidth
                 size="large"
-                sx={{ mt: 3, borderRadius: "50px",color:"white" }}
+                sx={{ mt: 3, borderRadius: "50px" }}
                 type="submit"
                 variant="contained"
-                
+                color="warning"
               >
                 {t("Create")}
               </Button>
